eb377e2c0d626e16effe6cbd3f59f8cb99266231
Gent Rexha <gent.rexha@siemens.com>
2019-08-23 10:40:05 +0200

    Finished preprocessing the data. Started creating .gifs

--- src/kosovo-population-animation.r
+++ src/kosovo-population-animation.r
@@ -1,9 +1,17 @@
-# Load data
 library(readr)
 library(dplyr)
+library(rgdal)
+library(ggplot2)
+library(maps)
+library(ggthemes)
+library(gganimate)
+library(hablar)
 
+# Load data
 url_csv <- '../data/population.csv'
 ks_pop <- read_csv(url_csv)
+ks_pop <- na.omit(ks_pop)
+ks_pop <- ks_pop %>% convert(int(year))
 
 # Set map
 setwd("C:/Projects/Personal/kosovo-population-animation/src/")
@@ -22,16 +30,21 @@ map <- ggplot() +
             aes(x = long, y = lat, group = group),
             color = 'gray', size = .2) + theme_map()
 
-# Draw points
-library(ggplot2)
-library(maps)
-library(ggthemes)
+# Draw points and animate
+# For more info:
+# https://github.com/thomasp85/gganimate
 
-map_w_points <- map +
+anim <- map +
   geom_point(aes(x = X, y = Y, size = population),
-             data = ks_pop[which(ks_pop$year=='31-12-2018'), ], alpha = .5) + 
+             data = ks_pop, alpha = .5, color = "steelblue") + 
   scale_size_continuous(range = c(1, 20), 
-                          breaks = c(10000, 25000, 50000, 100000)) + 
-  labs(size = 'population')
+                          breaks = c(10000, 25000, 50000, 100000, 200000)) + 
+  labs(size = 'Population', title="Year: {frame_time}") +
+  transition_time(year) +
+  ease_aes('cubic-in-out') +
+  enter_fade() +
+  exit_fade()
 
-print(map_w_points)
+anim
+
+anim_save("kosovo-population.gif", animation = last_animation())
