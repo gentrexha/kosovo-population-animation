2e8878e05069c08951d14694afe62853304440a4
Gent Rexha <gent.rexha@siemens.com>
2020-08-07 16:51:20 +0200

    Added missing files.

--- src/kosovo-population-animation-v2.r
+++ src/kosovo-population-animation-v2.r
@@ -14,13 +14,11 @@ library(transformr)
 library(magick)
 
 # set wd
-setwd("C:/Projects/Personal/kosovo-population-animation/src/")
+setwd("C:/Projects/Personal/kosovo-covid-data/src")
 
 # Load data
-url_csv <- "../data/population.csv"
+url_csv <- "../data/covid-komunat.csv"
 ks_pop <- read_csv(url_csv)
-ks_pop <- na.omit(ks_pop)
-ks_pop <- ks_pop %>% convert(int(year))
 
 # Set map
 shapefile <- readOGR(dsn = path.expand("../data/kosovo-shapefile"), 
@@ -30,32 +28,29 @@ shapefile <- readOGR(dsn = path.expand("../data/kosovo-shapefile"),
 # use in ggplot2
 shapefile_df <- fortify(shapefile, name = "XK_NAME")
 
-# Adding id to each row
-id_map <- data.frame(id = c(0:39), komuna = shapefile$XK_NAME, 
-    stringsAsFactors = FALSE)
-write.csv(id_map, "id_map.csv")
-
 # Add id to ks_pop, right?
 merged_df <- merge(shapefile_df, ks_pop, by = "id", all.x = TRUE)
 final_df <- merged_df[order(merged_df$order), ]
 
 # aggregate data to get mean latitude and mean longitude for
 # each state
-cnames <- aggregate(cbind(long, lat) ~ komuna_me_e, data = final_df, 
+cnames <- aggregate(cbind(long, lat) ~ komuna, data = final_df, 
     FUN = function(x) mean(range(x)))
 
 # new cpalette
-getPalette = colorRampPalette(brewer.pal(9, "Greens"))
+#getPalette = colorRampPalette(brewer.pal(9, "Greens"))
 
 # basic plot
-ggplot() + geom_polygon(data = final_df[which(final_df$year == 
-    1948), ], aes(x = long, y = lat, group = group, fill = population), 
-    color = "black", size = 0.25) + coord_map() + labs(title = "Population in Kosovo in 1948") + 
+ggplot() + geom_polygon(data = final_df[which(final_df$date == "2020-07-08"), ], 
+                        aes(x = long, y = lat, group = group, fill = new), 
+    color = "black", size = 0.25) + 
+    coord_map() + 
+    labs(title = "Population in Kosovo in 1948") +
     scale_fill_distiller(name = "Population", palette = "Greens", 
-        direction = 1, breaks = pretty_breaks(n = 7), limits = c(min(final_df$population, 
-            na.rm = TRUE), max(final_df$population, na.rm = TRUE))) + 
+        direction = 1, breaks = pretty_breaks(n = 7), limits = c(min(final_df$new, 
+        na.rm = TRUE), max(final_df$new, na.rm = TRUE))) + 
     theme_nothing(legend = TRUE) + geom_text(data = cnames, aes(long, 
-    lat, label = komuna_me_e), size = 3, fontface = "bold")
+        lat, label = komuna_me_e), size = 3, fontface = "bold")
 
 # Final plots
 for (year in c(1948:2018)) {
